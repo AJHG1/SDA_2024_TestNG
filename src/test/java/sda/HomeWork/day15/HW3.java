37b1ba4bcca44e122872e5362009f001d6b67550
AJHG059 <135741598+AJHG059@users.noreply.github.com>
2024-03-13 14:20:37 +0300

    cc

--- src/test/java/sda/HomeWork/day15/HW3.java
+++ src/test/java/sda/HomeWork/day15/HW3.java
@@ -44,5 +44,6 @@ public class HW3 extends TestBase {
         //Verify error message text is Your password is invalid!
         String errormassage = error.getText();
         Assert.assertEquals("Your password is invalid!" ,errormassage );
+//
     }
 }
