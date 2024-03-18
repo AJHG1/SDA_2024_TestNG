1748f92f2a7d5ed3678ae2f0f541a2221a8725b5
AJHG059 <135741598+AJHG059@users.noreply.github.com>
2024-03-18 13:31:13 +0300

    HW

---
+++ src/test/java/sda/HomeWork/day18/HW3.java
@@ -0,0 +1,11 @@
+package sda.HomeWork.day18;
+
+import sda.utilites.TestBase;
+
+public class HW3 extends TestBase {
+
+//    Day18HW:3
+//    Execute C04DependsOnMethods class, HardAssertionHomework and SoftAssertionHomework in parallel
+//            (If you find problem with class names you can select any three classes from your project)
+//    Thread num should be 3 and execution  should be done in "class" level
+}
